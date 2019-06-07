8f7e49069711f25e871cc5ed0b5a74fb38d12284
Jonathan Cohen <jonathancohen@MacBook-Pro-de-Jonathan-2.local>
2019-06-07 18:05:17 +0200

    integration section dna and specifications and js for main image and accordion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,40 @@
-console.log('\'Allo \'Allo!');
+/* FUNCTION FOR FLIP MAIN SPECIE IMAGE*/
 
+function change() {
+    let img1 = 'http://localhost:9000/images/beagle.jpg',
+        img2 = 'http://localhost:9000/images/beagle-chiot.jpg';
+    let txt1 = 'Chiot';
+    let txt2 = 'Adulte';
+
+    let imgElement = document.getElementById('img');
+    let txtElement = document.getElementById('txt');
+
+    imgElement.src = (imgElement.src === img1) ? img2 : img1;
+    txtElement.textContent = (txtElement.textContent === txt1) ? txt2 : txt1;
+}
+
+let mainImg = document.getElementById('flip');
+
+mainImg.addEventListener('click', function() {
+    change();
+});
+
+
+/* ACCORDION */
+
+var accordionWrapperNode = document.getElementById('specifications');
+var accordionHeadline = accordionWrapperNode.getElementsByTagName('h2');
+
+function accordionToggle (e) {
+    e.preventDefault();
+    var accordionContent = this.nextSibling.nextSibling;
+    if (accordionContent.classList.contains('hide')) {
+        accordionContent.className = '';
+    } else {
+        accordionContent.className = 'hide';
+    }
+}
+
+for (var i = 0; i < accordionHeadline.length; i++) {
+    accordionHeadline[i].addEventListener('click', accordionToggle);
+}
